76790d14c4a50bd687c61a7e5dcbfa4b29afcf32
subash7492 <subashpandit7492@gmail.com>
2024-07-30 23:38:25 +0530

    push code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
-import AmazonSettlements from "./Components/AmazonSettlements/AmazonSettlements";
-import Home from "./Components/Home/Home";
-import AccountCard from "./Components/OnboardingUpdate/AccountCard";
-import Dashboard from "./Components/Dashboard/Dashboard";
-// import OnboardingUpdate from "./Components/OnboardingUpdate/OnboardingUpdate";
-// import ContentComponent from "./Components/AmazonSettlements/ContentComponent";
 import NavbarComponent from "./../src/Components/Navbar/NavbarComponent"
 
 
 function App() {
   return (
     <div className="app">
-      {/* <Home /> */}
-      {/* <OnboardingUpdate /> */}
-      {/* <AccountCard/>
-      <AmazonSettlements />
-      <Dashboard/> */}
-      {/* <ContentComponent/> */}
 <NavbarComponent/>
 
     </div>
